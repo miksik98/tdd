5acbd912329891476d209df4d5a7c9dd32450b29
Mikołaj Sikora <mikolajsikora98@gmail.com>
2021-06-25 16:34:54 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "tdd"
+
+scalaVersion := "2.12.9"
+
+libraryDependencies ++= Seq(
+  "org.scalatest"             %% "scalatest"                 % "3.2.2" % "test"
+)
---
+++ src/test/scala/tdd/MultiplicationTest.scala
@@ -0,0 +1,9 @@
+package tdd
+
+class MultiplicationTest extends FunSuite with Matchers {
+  test("testMultiplication") {
+    Dollar fve = new Dollar(5)
+    five.times(2)
+    10 shouldBe five.ammount
+  }
+}
