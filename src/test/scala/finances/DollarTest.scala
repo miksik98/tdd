b0a550f2b9036ad9e210b76b5492d1ea2af4f348
misikora <mikolajsi@student.agh.edu.pl>
2021-06-25 20:13:58 +0200

    Chapter 4

--- src/test/scala/finances/DollarTest.scala
+++ src/test/scala/finances/DollarTest.scala
@@ -5,10 +5,8 @@ import org.scalatest.{FunSuite, Matchers}
 class DollarTest extends FunSuite with Matchers {
   test("testMultiplication") {
     val five = new Dollar(5)
-    val product1 = five.times(2)
-    product1.amount shouldBe 10
-    val product2 = five.times(3)
-    product2.amount shouldBe 15
+    five.times(2) shouldBe new Dollar(10)
+    five.times(3) shouldBe new Dollar(15)
   }
 
   test("testEquality") {
